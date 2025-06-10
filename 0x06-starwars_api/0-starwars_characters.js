d1577b5e71fcb1b18f9b3cf8b3ee342027759618
Laila2005 <laila.mf2005@gmail.com>
2025-06-10 12:13:20 +0300

    task done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (!movieId) process.exit();
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(url, (err, res, body) => {
-  if (err) return;
-
-  const characters = JSON.parse(body).characters;
-
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], (err, res, body) => {
-      if (!err) {
-        console.log(JSON.parse(body).name);
-      }
-    });
-  }
-});
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
